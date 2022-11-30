62b8abeab181bf2fd1ff446e7a4b737d5c0a267b
Noizzzz <noizzzz@yandex.ru>
2022-11-30 22:44:15 +0700

    Написан код для программы,которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

--- C#/HW-seminar1.2/Program.cs
+++ C#/HW-seminar1.2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int N = Convert.ToInt32(number);
+
+if (N<0)
+{
+    for (int i = -2; i >= N; i-=2)
+ {
+    Console.Write($"{i},");
+ }
+}
+
+
+if (N>0)
+{
+    for (int i = 2; i <= N; i+=2)
+ {
+    Console.Write($"{i},");
+ }
+}
+
+if (N==0)
+{
+    Console.WriteLine("0");
+}
