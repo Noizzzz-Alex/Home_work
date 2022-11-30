9a111e56154de258bf00ca5b3060e52d0abbf61a
Noizzzz <noizzzz@yandex.ru>
2022-11-30 19:14:26 +0700

    Create program #8

--- C#/HW-seminar1/Program.cs
+++ C#/HW-seminar1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+string numbers = Console.ReadLine();
+int N = Convert.ToInt32(numbers);
+
+if (N == 0)
+
+    Console.WriteLine("Неверно введено число");
+else
+{
+if (N % 2 == 0)
+    Console.WriteLine("Число четное");
+else
+    Console.WriteLine("Чиcло нечетное");
+}
+
