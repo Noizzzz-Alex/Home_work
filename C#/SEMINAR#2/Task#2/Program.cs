bde978d25f9e39726a4d0e8ccde7b04f7886597a
Noizzzz <noizzzz@yandex.ru>
2022-12-04 15:31:06 +0700

    Добавлено решение 2й задачи

--- C#/SEMINAR#2/Task#2/Program.cs
+++ C#/SEMINAR#2/Task#2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число");
+
+int numSTR1 = int.Parse(Console.ReadLine()??"");
+int dig = 0;
+
+if (100 <= numSTR1 && numSTR1 <= 999)
+{
+dig = numSTR1 % 10;
+Console.WriteLine($"Третья цифра числа = {dig}");
+}
+
+if (0 <= numSTR1 && numSTR1 <= 99)
+{
+Console.WriteLine("Третья цифра отсутсвует");
+}
+
+
+if (numSTR1 > 999)
+{
+while (numSTR1 > 999)
+    {
+        numSTR1 = numSTR1 / 10;
+    }
+numSTR1 = numSTR1 % 10;
+Console.WriteLine($"Третья цифра числа = {numSTR1}");
+}
