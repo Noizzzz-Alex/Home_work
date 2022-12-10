b81ad97e8f49e69334d4deed96abe535b76260c2
Noizzzz <noizzzz@yandex.ru>
2022-12-10 18:51:06 +0700

    Добавлено решение 1-й задачи семинара №4

---
+++ C#/Seminar#4/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+double GetNumber(string message)
+{
+    bool isCorrect = false;
+    double result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (double.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода числа. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+double number = GetNumber("введите число");
+double pow = GetNumber("в какую степень желаете возвести?");
+double multi = Math.Pow(number, pow);
+Console.WriteLine($"{number} ^{pow} = {multi}");
