1999a11821bb6e3bdac4de0faee3dcdbe27ce723
Noizzzz <noizzzz@yandex.ru>
2022-12-23 03:47:56 +0700

    Добавлено решение 2й задачи семинара №7

---
+++ C#/Seminar#7/Task#2/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+ и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+
+int GetNumber(string message)
+{
+    int result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число.");
+        }
+    }
+
+    return result;
+}
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+int line = GetNumber("Введите кол-во строк");
+int column = GetNumber("Введите кол-во столбцов");
+Console.WriteLine();
+int[,] matrix = InitMatrix(line, column);
+PrintArray(matrix);
+Console.WriteLine();
+
+int idLineElement = GetNumber("Элемент какой строки нужен?");
+int idColumnElement = GetNumber("Элемент какого столбца нужен?");
+
+if (idLineElement <= matrix.GetLength(0) && idColumnElement <= matrix.GetLength(1))
+{
+    Console.WriteLine($"Элемент найден = {matrix[idLineElement, idColumnElement]}");
+}
+else
+{
+    Console.WriteLine("Элемент отсутствует в массиве");
+}
+
+
+
