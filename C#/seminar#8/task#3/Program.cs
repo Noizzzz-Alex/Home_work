09f8be7fbd9c07d32668874c7405995cea0daeaf
Noizzzz <noizzzz@yandex.ru>
2022-12-25 20:48:53 +0700

    Добавлено решение 3-й задачи семинара №8

---
+++ C#/seminar#8/task#3/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+
+*/
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+int lineMatrix = 2;
+int columnMatrix =2;
+void MultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMatrix.GetLength(1); k++)
+      {
+        sum += firstMatrix[i,k] * secondMatrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+int[,] matrixOne = InitMatrix(lineMatrix, columnMatrix);
+int[,] matrixSecond = InitMatrix(lineMatrix, columnMatrix);
+int[,] resultMatrix = InitMatrix(lineMatrix, columnMatrix);
+Console.WriteLine();
+PrintArray(matrixOne);
+Console.WriteLine();
+PrintArray(matrixSecond);
+Console.WriteLine();
+MultiplyMatrix(matrixOne,matrixSecond,resultMatrix);
+PrintArray(resultMatrix);
