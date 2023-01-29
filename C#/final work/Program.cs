bbfb11b09f1bc45cdffbccb723460ac721134e9b
Noizzzz <noizzzz@yandex.ru>
2023-01-29 19:21:35 +0700

    Добавлены два массива, исходный и новый в который будем добавлять наши значения

--- C#/final work/Program.cs
+++ C#/final work/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5]{"edwedwe","ede","rfrfw","11d","a"};
+string[] arraynew = new string[array.Length];
