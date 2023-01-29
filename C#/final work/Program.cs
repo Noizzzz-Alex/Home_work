bec468cae2fd340119216829eeba832b3d7659e0
Noizzzz <noizzzz@yandex.ru>
2023-01-29 19:22:31 +0700

    Добавлен метод вывода массива в консоль

--- C#/final work/Program.cs
+++ C#/final work/Program.cs
@@ -1,2 +1,10 @@
 ﻿string[] array = new string[5]{"edwedwe","ede","rfrfw","11d","a"};
 string[] arraynew = new string[array.Length];
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i] } ");
+    }
+    Console.WriteLine();
+}
